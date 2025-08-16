d74238cb8a9c4c5698643e5695fb4a426009dcb3
leonardonunesz <leonardopsnunes@gmail.com>
2025-08-16 16:36:14 -0300

    redirect to home

---
+++ src/app/home/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <a href="home/"><button>clique aqui</button></a>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return
+export default function Page() {
+  return (
+    redirect('/home')
+  )
 }
